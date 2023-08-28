1d75f32c68758732f23af959215a20a66b488304
SWARAJ RAJENDRA MUTALIK <71939130+swarajmutalik@users.noreply.github.com>
2023-08-28 19:51:03 +0530

    Delete user.js

--- Major-1/models/user.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-const Schema = mongoose.Schema;
-
-const UserSchema = new Schema({
-  username: String,
-  password: String,
-});
-
-module.exports = mongoose.model("User", UserSchema);
