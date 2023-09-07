8437d7e50cdce288f19ac130111631d67d9b560f
SWARAJ RAJENDRA MUTALIK <71939130+swarajmutalik@users.noreply.github.com>
2023-09-08 00:56:58 +0530

    Updated register.js

--- Major-1/models/register.js
+++ Major-1/models/register.js
@@ -2,9 +2,22 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const RegisterSchema = new Schema({
-  username: String,
-  email: String,
-  password: String,
+  username: {
+    type: String,
+    required: [true, "Username cannot be left blank"],
+  },
+
+  email: {
+    type: String,
+    required: [true, "Email cannot be left blank"],
+  },
+
+  password: {
+    type: String,
+    required: [true, "Password cannot be left blank"],
+  },
+
 });
 
 module.exports = mongoose.model("Register", RegisterSchema);
+
